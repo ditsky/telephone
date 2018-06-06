89ea70ea5ae91c6a3383c7bcadf45ebddea7f965
ditsky <34258091+ditsky@users.noreply.github.com>
2018-06-06 15:05:03 -0400

    add responding pages no views yet

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/host');
+var usersRouter = require('./routes/join');
+var usersRouter = require('./routes/settings');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/host', usersRouter);
+app.use('/join', usersRouter);
+app.use('/settings', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
